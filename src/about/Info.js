4ba8154729287e3e525baaf8656a3d222186a6ef
sonya36 <soniyashrm399gmail.com>
2024-10-27 18:14:05 +0545

    updated jsonss

--- src/about/Info.js
+++ src/about/Info.js
@@ -6,17 +6,17 @@ const Info = () => {
             <div className="about_box">
                 <i className = "bx bx-award about_icon"></i>
                 <h3 className="about_title">Experience</h3>
-                <span className="about_subtitle">8 Years Working</span>
+                <span className="about_subtitle"></span>
             </div>
             <div className="about_box">
             <i className = "bx bx-briefcase-alt about_icon"></i>
                 <h3 className="about_title">Completed</h3>
-                <span className="about_subtitle">48 + Projects</span>
+                <span className="about_subtitle"></span>
             </div>
             <div className="about_box">
             <i className = "bx bx-support about_icon"></i>
-                <h3 className="about_title">Support</h3>
-                <span className="about_subtitle">Online 24/7</span>
+                <h3 className="about_title"></h3>
+                <span className="about_subtitle"></span>
             </div>
         </div>
 
